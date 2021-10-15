fb99b77ad26d65f070961762bc0da86f8de067d6
rentainhe <596106517@qq.com>
2021-10-15 10:58:34 +0800

    add benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,13 @@
+set -aux
+
+MODEL="alexnet"
+BATCH_SIZE=64
+DATA_PATH="/DATA/disk1/ImageNet/extract"
+IMG_SIZE=224
+NUM_WORKERS=8
+
+python benchmark.py --model $MODEL \
+                    --data_path $DATA_PATH \
+                    --batch_size $BATCH_SIZE \
+                    --img_size $IMG_SIZE \
+                    --num_workers $NUM_WORKERS
