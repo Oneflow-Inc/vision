18c68cf8a5926b759a23d4998c4cf0c4ecfbede6
rentainhe <596106517@qq.com>
2021-10-16 10:19:42 +0800

    add se_layer

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
-from flowvision.models import alexnet
-from one_utils import eval_flow_acc
+import flowvision.nn as nn
+import oneflow as flow
 
 
 
-# model = alexnet(pretrained=True)
+# # model = alexnet(pretrained=True)
 
-# eval_flow_acc(model, "/DATA/disk1/ImageNet/extract", img_size=384, num_workers=0)
+# # eval_flow_acc(model, "/DATA/disk1/ImageNet/extract", img_size=384, num_workers=0)
 
-from flowvision.models import ModelCreator
-# model = ModelCreator.create_model('vit_b_16_384', pretrained=True, model_dir="./test")
-# print(model)
-ModelCreator.model_table("vgg*")
+# from flowvision.models import ModelCreator
+# # model = ModelCreator.create_model('vit_b_16_384', pretrained=True, model_dir="./test")
+# # print(model)
+# ModelCreator.model_table("vgg*")
 
---
+++ flowvision/nn/__init__.py
@@ -0,0 +1,9 @@
+from .attention import SE
+from .blocks import ConvBn, ConvAct, ConvBnAct
+
+__all__ = [
+    "SE",
+    "ConvBn",
+    "ConvAct",
+    "ConvBnAct"
+]
