68f2d10eef8756fb5645690093b23b8427b7bd3d
rentainhe <596106517@qq.com>
2021-10-16 15:26:00 +0800

    add mobilenet v2 and update model zoo

--- flowvision/models/__init__.py
+++ flowvision/models/__init__.py
@@ -5,6 +5,7 @@ from .vgg import *
 from .mnasnet import *
 from .resnet import *
 from .shufflenet_v2 import *
+from .mobilenet_v2 import *
 
 from .utils import load_state_dict_from_url
 from .registry import ModelCreator
