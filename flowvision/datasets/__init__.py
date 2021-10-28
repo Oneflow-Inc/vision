913bc84956c79d8c338bd817c9148286583ab5f3
kaijieshi7 <1638961687@qq.com>
2021-10-28 10:32:54 +0800

    updata init

--- flowvision/datasets/__init__.py
+++ flowvision/datasets/__init__.py
@@ -19,6 +19,23 @@ from .coco import CocoCaptions, CocoDetection
 from .imagenet import ImageNet
 from .voc import VOCDetection, VOCSegmentation
 from .folder import DatasetFolder, ImageFolder
+from .fakedata import FakeData
+from .flickr import Flickr8k, Flickr30k
+from .inaturalist import INaturalist
+from .kitti import Kitti
+from .lfw import LFWPairs, LFWPeople
+from .lsun import LSUN, LSUNClass
+from .omniglot import Omniglot
+from .phototour import PhotoTour
+from .places365 import Places365
+from .sbd import SBDataset
+from .sbu import SBU
+from .semeion import SEMEION
+from .stl10 import STL10
+from .svhn import SVHN
+from .usps import USPS
+from .vision import VisionDataset
+from .widerface import WIDERFace
 
 __all__ = [
     "MNIST",
@@ -32,4 +49,24 @@ __all__ = [
     "VOCSegmentation",
     "DatasetFolder",
     "ImageFolder",
+    "FakeData",
+    "Flickr8k",
+    "Flickr30k",
+    "INaturalist",
+    "Kitti",
+    "LFWPairs",
+    "LFWPeople",
+    "LSUN",
+    "LSUNClass",
+    "Omniglot",
+    "PhotoTour",
+    "Places365",
+    "SBDataset",
+    "SBU",
+    "SEMEION",
+    "STL10",
+    "SVHN",
+    "USPS",
+    "VisionDataset",
+    "WIDERFace",
 ]
