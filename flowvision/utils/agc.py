7539b494f8f698c6a8ab434e04450fe204d3e0a4
rentainhe <596106517@qq.com>
2021-11-11 16:43:28 +0800

    update adaptive gradient clip

--- flowvision/utils/agc.py
+++ flowvision/utils/agc.py
@@ -13,8 +13,8 @@ def unitwise_norm(x, norm_type=2.0):
         # might need special cases for other weights (possibly MHA) where this may not be true
         return x.norm(norm_type, dim=tuple(range(1, x.ndim)), keepdim=True)
 
-# TODO: register tensor.mul_, tensor.clamp_ function
-# TODO: add test
+# TODO: Switch mul and clamp to inplace version
+# TODO: Add test
 def adaptive_clip_grad(parameters, clip_factor=0.01, eps=1e-3, norm_type=2.0):
     if isinstance(parameters, flow.Tensor):
         parameters = [parameters]
@@ -23,7 +23,7 @@ def adaptive_clip_grad(parameters, clip_factor=0.01, eps=1e-3, norm_type=2.0):
             continue
         p_data = p.detach()
         g_data = p.grad.detach()
-        max_norm = unitwise_norm(p_data, norm_type=norm_type).clamp_(min=eps).mul_(clip_factor)
+        max_norm = unitwise_norm(p_data, norm_type=norm_type).clamp(min=eps).mul(clip_factor)
         grad_norm = unitwise_norm(g_data, norm_type=norm_type)
         clipped_grad = g_data * (max_norm / grad_norm.clamp(min=1e-6))
         new_grads = flow.where(grad_norm < max_norm, g_data, clipped_grad)
