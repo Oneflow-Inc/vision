14f0e4b8cb350a8f8f143bc70c2e5264bbf69eeb
rentainhe <596106517@qq.com>
2021-11-13 11:15:59 +0800

    add SoftTargetCrossEntropy and add label-smooth test

--- flowvision/loss/cross_entropy.py
+++ flowvision/loss/cross_entropy.py
@@ -14,13 +14,21 @@ class LabelSmoothingCrossEntropy(nn.Module):
     
     def forward(self, x: flow.Tensor, target: flow.Tensor) -> flow.Tensor:
         # TODO: register F.log_softmax() function and switch flow.log(flow.softmax()) to F.log_softmax()
-        # logprobs = F.log_softmax(x, dim=-1)
-        probs = flow.softmax(x, dim=-1)
-        logprobs = flow.log(probs)
+        logprobs = flow.log_softmax(x, dim=-1)
         # TODO: fix gather bug when dim < 0
         # FIXME: only support cls task now
         nll_loss = -logprobs.gather(dim=1, index=target.unsqueeze(1))
         nll_loss = nll_loss.squeeze(1)
         smooth_loss = -logprobs.mean(dim=-1)
         loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+        return loss.mean()
+
+
+class SoftTargetCrossEntropy(nn.Module):
+
+    def __init__(self):
+        super(SoftTargetCrossEntropy, self).__init__()
+
+    def forward(self, x: flow.Tensor, target: flow.Tensor) -> flow.Tensor:
+        loss = flow.sum(-target * flow.log_softmax(x, dim=-1), dim=-1)
         return loss.mean()
