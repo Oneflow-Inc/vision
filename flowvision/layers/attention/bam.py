c0fe9d6c17228a43aee7cd3ba19e4542f0dd755c
rentainhe <596106517@qq.com>
2021-11-17 13:55:23 +0800

    add bam spatial attn module and whole bam module

--- flowvision/layers/attention/bam.py
+++ flowvision/layers/attention/bam.py
@@ -1,9 +1,9 @@
 import oneflow as flow
 import oneflow.nn as nn
 import oneflow.nn.functional as F
-import math
 
-from ..helpers import make_divisible
+from flowvision.layers import ConvBnAct
+from flowvision.models.helpers import make_divisible
 from flowvision.layers.build import LAYER_REGISTRY
 
 
@@ -44,3 +44,44 @@ class BamChannelAttn(nn.Module):
         b, c, _, _ = x.shape
         x_attn = self.gate(self.fc_layers(x.mean((2, 3)))).view(b, c, 1, 1)
         return x * x_attn.expand_as(x)
+
+
+class BamSpatialAttn(nn.Module):
+    def __init__(
+            self, channels, rd_ratio=1./16, rd_channels=None, rd_divisor=1, act_layer=nn.ReLU, gate_layer=nn.Sigmoid, num_layers=1, dilation=4, mlp_bias=False):
+        super(BamSpatialAttn, self).__init__()
+        if not rd_channels:
+            rd_channels = make_divisible(channels * rd_ratio, rd_divisor, round_limit=0.)
+        self.conv_layers = nn.Sequential()
+        for i in range(num_layers):
+            if i == 0:
+                self.conv_layers.add_module("conv_bn_act_%d" % i, ConvBnAct(channels, rd_channels, act_layer, kernel_size=3, padding=4, dilation=4, bias=mlp_bias))
+            else:
+                self.conv_layers.add_module("conv_bn_act_%d" % i, ConvBnAct(rd_channels, rd_channels, act_layer, kernel_size=3, padding=4, dilation=4, bias=mlp_bias))
+        self.conv_layers.add_module("conv_final", nn.Conv2d(rd_channels, 1, kernel_size=1, bias=mlp_bias))
+        self.gate = gate_layer()
+    
+    def forward(self, x):
+        b, c, _, _ = x.shape
+        x_attn = self.gate(self.conv_layers(x))
+        return x * x_attn.expand_as(x)
+
+
+@LAYER_REGISTRY.register()
+class BAM(nn.Module):
+    def __init__(self, channels, 
+                       rd_ratio=1./16, 
+                       rd_channels=None, 
+                       rd_divisor=1, 
+                       act_layer=nn.ReLU, 
+                       gate_layer=nn.Sigmoid, 
+                       num_channel_attn_layers=2,
+                       num_spatial_attn_layers=2, 
+                       mlp_bias=False):
+        super(BAM, self).__init__()
+        self.channel_att = BamChannelAttn(channels, rd_ratio, rd_channels, rd_divisor, act_layer, gate_layer, num_channel_attn_layers, mlp_bias)
+        self.spatial_att = BamSpatialAttn(channels, rd_ratio, rd_channels, rd_divisor, act_layer, gate_layer, num_spatial_attn_layers, mlp_bias)
+
+    def forward(self, x):
+        x_attn = 1 + F.sigmoid(self.channel_att(x) * self.spatial_att(x))
+        return x * x_attn
