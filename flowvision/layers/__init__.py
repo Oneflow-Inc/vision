0785bc0950daa0a231b665a50b0dbcd73ce5fbb5
simonJJJ <821898965@qq.com>
2021-11-24 19:45:35 +0800

    delete ctypes

--- flowvision/layers/__init__.py
+++ flowvision/layers/__init__.py
@@ -1,4 +1,3 @@
-import ctypes
 from .attention import *
 from .blocks import *
 from .regularization import *
@@ -9,16 +8,3 @@ from .helpers import *
 
 
 from .build import LAYER_REGISTRY, build_layers
-
-
-def lib_path():
-    import os
-    import glob
-
-    dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    libs = glob.glob(os.path.join(dir_path, "*.so"))
-    assert len(libs) > 0, f"no .so found in {dir_path}"
-    return libs[0]
-
-
-ctypes.CDLL(lib_path())
