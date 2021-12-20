b2156f62a6b68155f18872cb178bd1b607ac76af
kaijieshi7 <1638961687@qq.com>
2021-12-20 09:59:23 +0800

    add reference

--- projects/classification/benchmark.py
+++ projects/classification/benchmark.py
@@ -109,6 +109,8 @@ def accuracy(output, target, topk=(1,)):
 
     return [correct[:k].reshape(-1).float().sum(0) * 100.0 / batch_size for k in topk]
 
+
+# down json from https://github.com/google-research/reassessed-imagenet
 def accuracy_r(output, names, real_reables, topk=(1,)):
     '''
 
