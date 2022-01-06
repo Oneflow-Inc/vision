2a108326007bd01561c3160c85522f14a2666542
rentainhe <596106517@qq.com>
2022-01-07 01:25:27 +0800

    add plateau_lt

--- flowvision/scheduler/__init__.py
+++ flowvision/scheduler/__init__.py
@@ -3,3 +3,4 @@ from .cosine_lr import CosineLRScheduler
 from .linear_lr import LinearLRScheduler
 from .step_lr import StepLRScheduler
 from .multistep_lr import MultiStepLRScheduler
+from .plateau_lr import PlateauLRScheduler
