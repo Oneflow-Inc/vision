7b3caec51651c986ed9e3ff08549160e229bc80b
rentainhe <596106517@qq.com>
2022-01-13 14:25:27 +0800

    add README

--- projects/classification/ddp_training.sh
+++ projects/classification/ddp_training.sh
@@ -1,5 +1,8 @@
 export PYTHONPATH=$PWD:$PYTHONPATH
 set -aux
 
-python3 -m oneflow.distributed.launch --nproc_per_node 8 --master_port 12345  \
+GPU_NUMS=8
+MASTER_PORT=12345
+
+python3 -m oneflow.distributed.launch --nproc_per_node $GPU_NUMS --master_port $MASTER_PORT  \
         main.py --cfg configs/swin_tiny_patch4_window7_224.yaml \
