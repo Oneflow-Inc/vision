0d10e1e2547c83dacef74f527b043d2f2c9d0377
rentainhe <596106517@qq.com>
2022-01-22 01:50:31 +0800

    delete plateau lr

--- flowvision/scheduler/__init__.py
+++ flowvision/scheduler/__init__.py
@@ -3,6 +3,5 @@ from .cosine_lr import CosineLRScheduler
 from .linear_lr import LinearLRScheduler
 from .step_lr import StepLRScheduler
 from .multistep_lr import MultiStepLRScheduler
-from .plateau_lr import PlateauLRScheduler
 from .poly_lr import PolyLRScheduler
 from .tanh_lr import TanhLRScheduler
