d8c8c2dd3dadc04b8aae1b0fdc4bb8dd3002697f
Li Xiang <54010254+lixiang007666@users.noreply.github.com>
2022-01-21 15:25:15 +0800

    Sphinx document list collapse (#119)
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * Final

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,6 +58,10 @@ exclude_patterns = []
 #
 html_theme = "furo"
 
+def setup(app):
+    app.add_css_file( "css/sidebar.css" )
+    app.add_js_file("collapse.js")
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
