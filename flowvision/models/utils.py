a811f860023680a634de88ffea8e6120c904b95e
rentainhe <596106517@qq.com>
2022-01-26 01:12:44 +0800

    support delete .zip and .tar.gz file

--- flowvision/models/utils.py
+++ flowvision/models/utils.py
@@ -47,7 +47,7 @@ def _is_legacy_tar_format(filename):
     return tarfile.is_tarfile(filename)
 
 
-def _legacy_tar_load(filename, model_dir, map_location):
+def _legacy_tar_load(filename, model_dir, map_location, delete_tar_file=True):
     with tarfile.open(filename) as f:
         members = f.getnames()
         extracted_name = members[0]
@@ -55,6 +55,8 @@ def _legacy_tar_load(filename, model_dir, map_location):
         if not os.path.exists(model_dir):
             os.mkdir(model_dir)
         f.extractall(model_dir)
+    if delete_tar_file:
+        os.remove(filename)
     return flow.load(extracted_file)
 
 
@@ -62,7 +64,7 @@ def _is_legacy_zip_format(filename):
     return zipfile.is_zipfile(filename)
 
 
-def _legacy_zip_load(filename, model_dir, map_location):
+def _legacy_zip_load(filename, model_dir, map_location, delete_zip_file=True):
     # Note: extractall() defaults to overwrite file if exists. No need to clean up beforehand.
     #       We deliberately don't handle tarfile here since our legacy serialization format was in tar.
     with zipfile.ZipFile(filename) as f:
@@ -72,9 +74,9 @@ def _legacy_zip_load(filename, model_dir, map_location):
         if not os.path.exists(extracted_file):
             os.mkdir(extracted_file)
         f.extractall(model_dir)
-    # TODO: flow.load doesn't have map_location
-    # return flow.load(extracted_file, map_location=map_location)
-    return flow.load(extracted_file)
+    if delete_zip_file:
+        os.remove(filename)
+    return flow.load(extracted_file, map_location)
 
 
 def load_state_dict_from_url(
@@ -84,6 +86,7 @@ def load_state_dict_from_url(
     progress=True,
     check_hash=False,
     file_name=None,
+    delete_file=True,
 ):
     r"""Loads the OneFlow serialized object at the given URL.
 
@@ -105,11 +108,8 @@ def load_state_dict_from_url(
             ensure unique names and to verify the contents of the file.
             Default: ``False``
         file_name (string, optional): name for the downloaded file. Filename from `url` will be used if not set
-
+        delete_file (bool, optional): delete downloaded `.zip` file or `.tar.gz` file after unzipping them.
     """
-
-    if map_location is not None:
-        warnings.warn("Map location is not supported yet.")
 
     try:
         model_dir = get_cache_dir(model_dir)
@@ -135,9 +135,9 @@ def load_state_dict_from_url(
         download_url_to_file(url, cached_file, hash_prefix, progress=progress)
 
     if _is_legacy_zip_format(cached_file):
-        return _legacy_zip_load(cached_file, model_dir, map_location)
+        return _legacy_zip_load(cached_file, model_dir, map_location, delete_file)
     elif _is_legacy_tar_format(cached_file):
-        return _legacy_tar_load(cached_file, model_dir, map_location)
+        return _legacy_tar_load(cached_file, model_dir, map_location, delete_file)
     else:
         state_dict = flow.load(cached_file)
         return state_dict
