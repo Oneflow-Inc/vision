e1fd3ca1cb5f19ff0bd8bd817958f2b478ed6aea
59rentainhe <596106517@qq.com>
2022-02-01 17:08:22 +0000

    add helpers

---
+++ flowvision/layers/helpers.py
@@ -0,0 +1,10 @@
+"""Layer helpers
+"""
+
+def make_divisible(v, divisor=8, min_value=None, round_limit=0.9):
+    min_value = min_value or divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < round_limit * v:
+        new_v += divisor
+    return new_v
