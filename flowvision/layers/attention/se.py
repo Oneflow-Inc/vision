8a5aff5637fdbf575f3fd85584212f4cf7183198
Aries-chen <1416535234@qq.com>
2022-02-10 14:25:23 +0800

    add SEModule docstring

--- flowvision/layers/attention/se.py
+++ flowvision/layers/attention/se.py
@@ -3,6 +3,19 @@ import oneflow.nn as nn
 
 
 class SEModule(nn.Module):
+    """
+    A "Squeeze-and-Excitation" block that adaptively recalibrates channel-wise feature responses. This is based on
+    `"Squeeze-and-Excitation Networks" <https://arxiv.org/abs/1709.01507>`_. This unit is designed to improve the representational capacityof a network by enabling it to perform dynamic channel-wise feature recalibration.
+
+    Args:
+        channels (int): size of each input sample 
+        reduction (int): ratio that allows us tovary the capacity and computational cost of the SE model. Default: 16
+        rd_channels (int or None): the floor division of channels and reduction
+        act_layer (flow.nn.Module): a simple acting mechanism is achieved by using a relu activation 
+        gate_layer (flow.nn.Module): a simple gating mechanism is achieved by using a sigmoid activation 
+        mlp_bias (bool): if True, adds a learnable bias to the output. Default: False
+    """
+
     def __init__(
         self,
         channels,
---
+++ flowvision/version.py
@@ -0,0 +1,2 @@
+__version__ = '0.1.0'
+git_version = '59e674fa825485a83e67910876c5d7d1bc06872c'
