1bb4cf1e256acde5e0c98cdf270037bf6240d61f
hihippie <chiziqiu0923@gmail.com>
2022-02-21 15:44:08 +0800

    import resnest

--- flowvision/models/__init__.py
+++ flowvision/models/__init__.py
@@ -24,6 +24,7 @@ from .res2net import *
 from .efficientnet import *
 from .vision_transformer import *
 from .convnext import *
+from .resnest import *
 
 from . import neural_style_transfer
 from . import detection
