1b9bf550a5a4e0dda3243786e7613e0ad3411766
howin98 <1171760467@qq.com>
2022-03-13 16:41:19 +0800

    add example

--- benchmark/test_benchmark.py
+++ benchmark/test_benchmark.py
@@ -1,7 +1,10 @@
 from resnest import *
 
-def test_resnest(benchmark):
+def test_resnest50(benchmark):
     benchmark(run_resnest50)
+def test_resnest101(benchmark):
     benchmark(run_resnest101)
+def test_resnest200(benchmark):
     benchmark(run_resnest200)
+def test_resnest269(benchmark):
     benchmark(run_resnest269)
