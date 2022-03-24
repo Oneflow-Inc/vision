9e753803f6612692103d4c4582b98a208c9e2d84
hihippie <chiziqiu0923@gmail.com>
2022-03-24 04:24:40 +0000

    Delete unnecessary comments

--- flowvision/transforms/functional_tensor.py
+++ flowvision/transforms/functional_tensor.py
@@ -557,7 +557,6 @@ def _get_gaussian_kernel2d(
 ) -> Tensor:
     kernel1d_x = _get_gaussian_kernel1d(kernel_size[0], sigma[0]).to(device, dtype=dtype)
     kernel1d_y = _get_gaussian_kernel1d(kernel_size[1], sigma[1]).to(device, dtype=dtype)
-    # kernel2d = flow.mm(kernel1d_y[:, None], kernel1d_x[None, :])
     kernel2d = flow.matmul(kernel1d_y[:, None], kernel1d_x[None, :])
 
     return kernel2d
