2ebe5d430b11fb672c6b7936df31c526bc97d055
howin98 <1171760467@qq.com>
2022-04-07 11:48:59 +0800

    pub

--- benchmark/__init__.py
+++ benchmark/__init__.py
@@ -37,11 +37,12 @@ def compare_args(args):
         func_name = func.__name__
         file_name = sys._getframe().f_back.f_code.co_filename
         print('oneflow-benchmark-function::', end='')
-        print({
+        collect_info = json.dumps({
             'func_name': func_name,
             'file_name': file_name,
             'args': args
             })
+        print(collect_info)
         @gc_wrapper
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
