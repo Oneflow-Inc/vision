417cf54967db2c8ca7aa1ac46ed863e6ac63b40f
howin98 <1171760467@qq.com>
2022-04-10 09:23:24 +0800

    pub

--- benchmark/test_mobilenet.py
+++ benchmark/test_mobilenet.py
@@ -4,61 +4,61 @@ from flowvision.models.mobilenet_v3 import mobilenet_v3_large
 from flowvision.models.mobilenet_v2 import mobilenet_v2
 
 
-@oneflow_benchmark.ci_settings(compare={"median": "3%"})
+@oneflow_benchmark.ci_settings(compare={"median": "5%"})
 def test_mobilenet_v3_large_batch_size1(benchmark, net=mobilenet_v3_large, input_shape=[1, 3, 224, 224]):
     model, x, optimizer = fetch_args(net, input_shape)
     benchmark(run, model, x, optimizer)
 
 
-@oneflow_benchmark.ci_settings(compare={"median": "3%"})
+@oneflow_benchmark.ci_settings(compare={"median": "5%"})
 def test_mobilenet_v3_large_batch_size2(benchmark, net=mobilenet_v3_large, input_shape=[2, 3, 224, 224]):
     model, x, optimizer = fetch_args(net, input_shape)
     benchmark(run, model, x, optimizer)
 
 
-@oneflow_benchmark.ci_settings(compare={"median": "3%"})
+@oneflow_benchmark.ci_settings(compare={"median": "5%"})
 def test_mobilenet_v3_large_batch_size4(benchmark, net=mobilenet_v3_large, input_shape=[4, 3, 224, 224]):
     model, x, optimizer = fetch_args(net, input_shape)
     benchmark(run, model, x, optimizer)
 
 
-@oneflow_benchmark.ci_settings(compare={"median": "3%"})
+@oneflow_benchmark.ci_settings(compare={"median": "5%"})
 def test_mobilenet_v3_large_batch_size8(benchmark, net=mobilenet_v3_large, input_shape=[8, 3, 224, 224]):
     model, x, optimizer = fetch_args(net, input_shape)
     benchmark(run, model, x, optimizer)
 
 
-@oneflow_benchmark.ci_settings(compare={"median": "3%"})
+@oneflow_benchmark.ci_settings(compare={"median": "5%"})
 def test_mobilenet_v3_large_batch_size16(benchmark, net=mobilenet_v3_large, input_shape=[16, 3, 224, 224]):
     model, x, optimizer = fetch_args(net, input_shape)
     benchmark(run, model, x, optimizer)
 
 
-@oneflow_benchmark.ci_settings(compare={"median": "3%"})
+@oneflow_benchmark.ci_settings(compare={"median": "5%"})
 def test_mobilenet_v2_batch_size1(benchmark, net=mobilenet_v2, input_shape=[1, 3, 224, 224]):
     model, x, optimizer = fetch_args(net, input_shape)
     benchmark(run, model, x, optimizer)
 
 
-@oneflow_benchmark.ci_settings(compare={"median": "3%"})
+@oneflow_benchmark.ci_settings(compare={"median": "5%"})
 def test_mobilenet_v2_batch_size2(benchmark, net=mobilenet_v2, input_shape=[2, 3, 224, 224]):
     model, x, optimizer = fetch_args(net, input_shape)
     benchmark(run, model, x, optimizer)
 
 
-@oneflow_benchmark.ci_settings(compare={"median": "3%"})
+@oneflow_benchmark.ci_settings(compare={"median": "5%"})
 def test_mobilenet_v2_batch_size4(benchmark, net=mobilenet_v2, input_shape=[4, 3, 224, 224]):
     model, x, optimizer = fetch_args(net, input_shape)
     benchmark(run, model, x, optimizer)
 
 
-@oneflow_benchmark.ci_settings(compare={"median": "3%"})
+@oneflow_benchmark.ci_settings(compare={"median": "5%"})
 def test_mobilenet_v2_batch_size8(benchmark, net=mobilenet_v2, input_shape=[8, 3, 224, 224]):
     model, x, optimizer = fetch_args(net, input_shape)
     benchmark(run, model, x, optimizer)
 
 
-@oneflow_benchmark.ci_settings(compare={"median": "3%"})
+@oneflow_benchmark.ci_settings(compare={"median": "5%"})
 def test_mobilenet_v2_batch_size16(benchmark, net=mobilenet_v2, input_shape=[16, 3, 224, 224]):
     model, x, optimizer = fetch_args(net, input_shape)
     benchmark(run, model, x, optimizer)
