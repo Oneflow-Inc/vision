3077fa7c6a8f079fce1014e791f4b5a04dc6fef2
59rentainhe <596106517@qq.com>
2022-04-12 07:01:58 +0000

    reverse

--- projects/classification/models/build.py
+++ projects/classification/models/build.py
@@ -1,11 +1,7 @@
 from flowvision.models import ModelCreator
-from .fast_resnet50 import fast_resnet50
 
 
 def build_model(config):
     model_arch = config.MODEL.ARCH
-    if model_arch == "fast_resnet50":
-        model = fast_resnet50()
-    else:
-        model = ModelCreator.create_model(model_arch, pretrained=config.MODEL.PRETRAINED)
+    model = ModelCreator.create_model(model_arch, pretrained=config.MODEL.PRETRAINED)
     return model
