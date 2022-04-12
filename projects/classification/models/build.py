cc349ad8f6121bd7344640433d91db500e0a169c
59rentainhe <596106517@qq.com>
2022-04-12 06:51:30 +0000

    add basic resnet50 models

--- projects/classification/models/build.py
+++ projects/classification/models/build.py
@@ -1,7 +1,11 @@
 from flowvision.models import ModelCreator
+from .fast_resnet50 import fast_resnet50
 
 
 def build_model(config):
     model_arch = config.MODEL.ARCH
-    model = ModelCreator.create_model(model_arch, pretrained=config.MODEL.PRETRAINED)
+    if model_arch == "fast_resnet50":
+        model = fast_resnet50()
+    else:
+        model = ModelCreator.create_model(model_arch, pretrained=config.MODEL.PRETRAINED)
     return model
