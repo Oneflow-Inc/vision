bc9d22fdea248adb2f6055057a4591567ae3ace2
daquexian <daquexian566@gmail.com>
2022-04-22 11:16:57 +0800

    fix transform bug in another way
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- flowvision/transforms/functional.py
+++ flowvision/transforms/functional.py
@@ -412,8 +412,9 @@ def normalize(
         tensor = tensor.clone()
 
     dtype = tensor.dtype
-    std = np.array(std, dtype=flow.framework.dtype.convert_oneflow_dtype_to_numpy_dtype(dtype))
-    if (std == 0).any():
+    # NOTE: np array cannot be used as flow.as_tensor argument because of oneflow bug
+    np_dtype = flow.framework.dtype.convert_oneflow_dtype_to_numpy_dtype(dtype)
+    if (np.array(std, dtype=np_dtype) == 0).any():
         raise ValueError(
             "std evaluated to zero after conversion to {}, leading to division by zero.".format(
                 dtype
