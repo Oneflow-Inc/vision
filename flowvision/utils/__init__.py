aba75238f0511ee0ee2e2dcd7670dbdbf4d5387f
luyang <flowingsun007@163.com>
2022-11-29 03:22:12 +0000

    move utils.py to utils

--- flowvision/utils/__init__.py
+++ flowvision/utils/__init__.py
@@ -3,3 +3,4 @@ from .clip_grad import dispatch_clip_grad
 from .vision_helpers import make_grid, save_image
 from .metrics import AverageMeter, accuracy
 from .model_ema import ModelEmaV2
+from .utils import draw_bounding_boxes
