f64c07637e41a10b10d21e52c4d45b5781b691f3
BBuf <1182563586@qq.com>
2023-04-29 05:37:04 +0000

    add vision.ops.boxes module

---
+++ flowvision/ops/__init__.py
@@ -0,0 +1,13 @@
+from .boxes import (
+    batched_nms,
+    box_area,
+    box_convert,
+    box_iou,
+    clip_boxes_to_image,
+    complete_box_iou,
+    distance_box_iou,
+    generalized_box_iou,
+    masks_to_boxes,
+    nms,
+    remove_small_boxes,
+)
---
+++ flowvision/version.py
@@ -0,0 +1,2 @@
+__version__ = '0.2.1'
+git_version = '0364d4930c617fd7ca81f3dad940e0b0387064e5'
